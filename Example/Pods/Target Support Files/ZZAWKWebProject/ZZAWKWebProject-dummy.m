f1b16d97936925652ac7d9eab37b28d69f7af703
zhulei <zhulei@oradt.com>
2018-04-25 10:13:30 +0800

    Initial commot of Library
    Signed-off-by: zhulei <zhulei@oradt.com>

---
+++ Example/Pods/Target Support Files/ZZAWKWebProject/ZZAWKWebProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZZAWKWebProject : NSObject
+@end
+@implementation PodsDummy_ZZAWKWebProject
+@end
---
+++ ZZAWKWebProject/Classes/ZAWKViewController.h
@@ -0,0 +1,15 @@
+//
+//  ZAWKViewController.h
+//  H5-Demo-wk
+//
+//  Created by zhulei on 2018/4/9.
+//  Copyright © 2018年 zs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZAWKViewController : UIViewController
+
+-(instancetype)initWithURL:(NSString *)loadingURL WithLocalFilePath:(NSString *)filePath;
+
+@end
